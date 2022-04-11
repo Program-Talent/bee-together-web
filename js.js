fd46cc5cbccef6180d0ac728590370c6cd15958d
Felipe Haag de Lima <41767792+felipeh44g@users.noreply.github.com>
2022-04-10 22:31:35 -0300

    boa parte da playlist

--- js.js
+++ js.js
@@ -1,18 +1,48 @@
-function aaa (){
-    alert('a')
-}
 window.onload = function () {
+
+    function init() {
+        gapi.load('auth2', function() {
+            client_id: 'AIzaSyD1LnJfUsRtYAn9wMTMVdAqDi_TbKayWBM'
+        });
+      }
+
+      
     var example2 = new Vue({
-        el: '#overview',
+        el: '#ossoPesquisa',
         data: {
-            criterioPesquisa: ""
+            criterioPesquisa: "",
+            apresentaMusica: "none",
+            apresentaPlaylist: "inline"
         },
         // define métodos dentro do objeto `methods`
         methods: {
-            teste: function (event) {
-                this.criterioPesquisa = "";
+            adicionaMusica: function (event) {
+                this.apresentaMusica = "inline";
+                this.apresentaPlaylist = "none";
+            },
+            voltaPlaylist: function (event) {
+                this.apresentaPlaylist = "inline";
+                this.apresentaMusica = "none";
             }
         }
     })
 }
+
+// After the API loads, call a function to enable the search box.
+function handleAPILoaded() {
+    $('#search-button').attr('disabled', false);
+  }
   
+  // Search for a specified string.
+  function search() {
+    var q = $('#barraPesquisa').val();
+    var request = gapi.client.youtube.search.list({
+      q: q,
+      part: 'snippet'
+    });
+  
+    request.execute(function(response) {
+      var str = JSON.stringify(response.result);
+      $('#resultadoPesquisa').html('<pre>' + str + '</pre>');
+    });
+  }
